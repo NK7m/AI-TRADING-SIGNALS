be0e3dde34492acb701ad0dae569334a590924c6
NAITIK UPADHYAY <144269081+NK7m@users.noreply.github.com>
2025-08-30 16:02:25 +0530

    Delete utils directory

--- utils/config.py
+++
@@ -1,8 +0,0 @@
-def load_config():
-    return {
-        "asset": "BTC-USD",
-        "interval": "1h",
-        "discord_token": "YOUR_DISCORD_TOKEN",
-        "telegram_token": "YOUR_TELEGRAM_TOKEN",
-        # Add more config as needed
-    }
